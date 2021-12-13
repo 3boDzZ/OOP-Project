8e37459365618be79c5b20cca7ce9f8928a6cb01
Abdelrahman Mohamed <67402039+3boDzZ@users.noreply.github.com>
2021-12-13 21:08:44 +0200

    Add files via upload

---
+++ Resala/Resala/index.php
@@ -0,0 +1,7 @@
+<?php
+        // put your code here
+        include_once 'ClientView';
+        $ClientView = new ClientView();
+        $ClientView->selectUserType();
+        echo 'Hello';
+?>  
