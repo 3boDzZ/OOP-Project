d7e40b25e587590ec13e2966842b92c96e38b6f6
Khaled5406 <92303237+Khaled5406@users.noreply.github.com>
2022-01-24 20:38:59 +0200

    Add files via upload

---
+++ CS314Pro/config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+class config {
+    const DB_HOST= 'localhost';
+    const DB_NAME= 'root';
+    const DB_USER = 'cs 314 project';
+    const DB_PASSWORD =' ';
+    const DB_ERRORS = true;
+}
+?>;
---
+++ CS314Pro/index.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Front controller
+ *
+ * PHP version 7.0
+ */
+
+ ini_set('session.cookie_lifetime', '864000'); // ten days in seconds
+
+/**
+ * Composer
+ */  
+require '../vendor/autoload.php';
+
+/**
+ * Error and Exception handling
+ */
+
+error_reporting(E_ALL);
+set_error_handler('Core\Error::errorHandler');
+set_exception_handler('Core\Error::exceptionHandler');
+
+
+/**
+ *  Sessions 
+ */
+session_start();
+
+/**
+ * Routing
+ */
+$router = new Core\Router();
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('login', ['controller' => 'Login', 'action' => 'new']);
+$router->add('logout', ['controller' => 'Login', 'action' => 'destroy']);
+$router->add('{controller}/{action}');
+    
+$router->dispatch($_SERVER['QUERY_STRING']);
